a25a4b8382dd2c93f8b418685f766ca8cc8cf072
Reaper <149217292+ReaperGrimWatlz@users.noreply.github.com>
2024-07-23 17:59:52 +0530

    Update Adding to df.py

--- AI/Adding to df.py
+++ AI/Adding to df.py
@@ -9,4 +9,5 @@ import pandas as pd
 xdf1=pd.DataFrame({'Name':['John','Jane'],'Age':[25,20]})
 xdf2=pd.DataFrame({'Name':['Peter','Paul'],'Age':[35,30]})
 xdf3=pd.DataFrame({'Name':['Tony','Pepper'],'Age':[25,20]})
-xdf=pd.concat
+xdf=pd.concat([xdf1,xdf2,xdf3],ignore_index=True)
+print(xdf)
