d3e5c1f238673c1a79858259c361873517f1c9f5
Reaper <149217292+ReaperGrimWatlz@users.noreply.github.com>
2024-07-23 18:10:15 +0530

    Update Adding columns to df.py

--- AI/Adding columns to df.py
+++ AI/Adding columns to df.py
@@ -1,6 +1,7 @@
 import pandas as pd
-data={'R.No.':[1,2,3,4,5,'Name':['A','B','C','D','E'],'marks':[99,80,89,90,91]}
+data={'R.No.':[1,2,3,4,5],'Name':['A','B','C','D','E'],'marks':[99,80,89,90,91]}
 xdf=pd.DataFrame(data)
 print(xdf)
-xdf['Percentage']=((xdf['marks']/100)*100,'%')
+xdf['Percentage']=(xdf['marks']/100)*100
+xdf['%']='%'
 print(xdf)
